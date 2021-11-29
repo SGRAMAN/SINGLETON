7d42b80ade0a634556fa6bce1071bcfb3bcda990
SGRAMAN <sgraman.subramanian@gmail.com>
2021-11-29 19:21:56 +0530

    commit

---
+++ src/org/single/Practice.java
@@ -0,0 +1,45 @@
+package org.single;
+
+public class Practice {
+	
+	static Practice a;
+	
+	public static Practice get() {
+		int before = System.identityHashCode(a);
+		System.out.println(before);
+		if (a==null) {
+			a = new Practice();
+			int after = System.identityHashCode(a);
+			
+			System.out.println(after);
+			
+		}
+		return a;
+
+	}
+	private void empName() {
+		System.out.println("SGR");
+
+	}
+	public static void main(String[] args) {
+		
+		Practice practice = get();
+		int identityHashCode = System.identityHashCode(practice);
+		System.out.println(identityHashCode);
+		
+		Practice practice2 = get();
+		int identityHashCode2 = System.identityHashCode(practice2);
+		System.out.println(identityHashCode2);
+		
+		a.empName();
+		int after1 = System.identityHashCode(a);
+		System.out.println(after1);
+		
+		practice.empName();
+		
+		
+		
+		
+	}
+
+}
